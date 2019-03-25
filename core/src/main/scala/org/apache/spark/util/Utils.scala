dd4656cc0c2320d23a2d618f86b3c69ae56ca8be
mcheah <mcheah@palantir.com>
2019-03-25 16:34:09 -0700

    Hack around kernel bug

--- core/src/main/scala/org/apache/spark/util/Utils.scala
+++ core/src/main/scala/org/apache/spark/util/Utils.scala
@@ -340,7 +340,11 @@ private[spark] object Utils extends Logging {
       output: WritableByteChannel,
       startPosition: Long,
       bytesToCopy: Long): Unit = {
-//  val initialPos = output.position()
+    val outputInitialState = output match {
+      case outputFileChannel: FileChannel =>
+        Some((outputFileChannel.position(), outputFileChannel))
+      case _ => None
+    }
     var count = 0L
     // In case transferTo method transferred less data than we have required.
     while (count < bytesToCopy) {
@@ -349,21 +353,23 @@ private[spark] object Utils extends Logging {
     assert(count == bytesToCopy,
       s"request to copy $bytesToCopy bytes, but actually copied $count bytes.")
 
-//    // Check the position after transferTo loop to see if it is in the right position and
-//    // give user information if not.
-//    // Position will not be increased to the expected length after calling transferTo in
-//    // kernel version 2.6.32, this issue can be seen in
-//    // https://bugs.openjdk.java.net/browse/JDK-7052359
-//    // This will lead to stream corruption issue when using sort-based shuffle (SPARK-3948).
-//    val finalPos = output.position()
-//    val expectedPos = initialPos + bytesToCopy
-//    assert(finalPos == expectedPos,
-//      s"""
-//         |Current position $finalPos do not equal to expected position $expectedPos
-//         |after transferTo, please check your kernel version to see if it is 2.6.32,
-//         |this is a kernel bug which will lead to unexpected behavior when using transferTo.
-//         |You can set spark.file.transferTo = false to disable this NIO feature.
-//           """.stripMargin)
+    // Check the position after transferTo loop to see if it is in the right position and
+    // give user information if not.
+    // Position will not be increased to the expected length after calling transferTo in
+    // kernel version 2.6.32, this issue can be seen in
+    // https://bugs.openjdk.java.net/browse/JDK-7052359
+    // This will lead to stream corruption issue when using sort-based shuffle (SPARK-3948).
+    outputInitialState.foreach { case (initialPos, outputFileChannel) =>
+      val finalPos = outputFileChannel.position()
+      val expectedPos = initialPos + bytesToCopy
+      assert(finalPos == expectedPos,
+        s"""
+           |Current position $finalPos do not equal to expected position $expectedPos
+           |after transferTo, please check your kernel version to see if it is 2.6.32,
+           |this is a kernel bug which will lead to unexpected behavior when using transferTo.
+           |You can set spark.file.transferTo = false to disable this NIO feature.
+         """.stripMargin)
+    }
   }
 
   /**
